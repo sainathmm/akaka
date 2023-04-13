b9a141ff64e07b068413e0392aa8b3ee2744fdb9
sainathmm <130647122+sainathmm@users.noreply.github.com>
2023-04-13 14:46:29 +0530

    Update new.java

--- new.java
+++ new.java
@@ -1,5 +1,12 @@
-public class new{
- public static void main(String[] args){
-      System.out.println("this is the printed statement");
- }
- }
+class HelloWorld {
+    public static void main(String[] args) {
+       int a = 10;
+       int b = 20;
+       if(a==b){
+           System.out.println("it is a square");
+       }
+       else{
+           System.out.println("it is a rectangle");
+       }
+    }
+}
